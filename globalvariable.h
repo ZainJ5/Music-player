dbb024941613343f8f7171fd12e2cc96562d5760
ZainJ5 <154011623+ZainJ5@users.noreply.github.com>
2024-05-08 00:31:07 +0500

    Add files via upload

---
+++ globalvariable.h
@@ -0,0 +1,26 @@
+// global.h
+
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#include <string>
+extern std::string globalVariable;
+extern int nameline1;
+extern int totalvideos;
+extern int casize;
+extern int cvideosize;
+extern int lineofusername;
+extern int gage;
+extern int videolikes;
+extern int videoviews;
+extern std::string createrpassword;
+extern std::string username;
+extern std::string createrviews;
+extern std::string createrlikes;
+extern int createrrevenue;
+extern std::string *videonamesarray;
+extern std::string selectedvideo;
+extern std::string videopath;
+extern std::string *Allcreaters;
+extern std::string *CreatersVideo;
+#endif // GLOBAL_H

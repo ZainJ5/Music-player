5d17897cc4a01daa49dc017c2a10c0e9c41b6f04
ZainJ5 <154011623+ZainJ5@users.noreply.github.com>
2024-05-08 00:30:45 +0500

    Add files via upload

---
+++ globalvariable.cpp
@@ -0,0 +1,20 @@
+#include <string>
+std::string globalVariable = "";
+int nameline1;
+int totalvideos;
+int gage;
+int videolikes;
+int videoviews;
+std::string createrviews;
+std::string createrlikes;
+int createrrevenue;
+std::string *videonamesarray;
+std::string selectedvideo;
+std::string videopath;
+std::string *Allcreaters;
+std::string username;
+int casize;
+int cvideosize;
+std::string *CreatersVideo;
+int lineofusername;
+std::string createrpassword;
